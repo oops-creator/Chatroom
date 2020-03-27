d91a9f6addfc9d580bdb6143c4fb0913a80aa40b
oops-creator <still.shivam@gmail.com>
2020-03-28 01:02:09 +0530

    Add layout for chat space

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
+const http = require('http')
 const hbs = require('hbs')
+const socketio = require('socket.io')
 
 
 //--------------------------------------------------
@@ -9,8 +11,10 @@ const partialPath = path.join(__dirname , './templates/partials')
 const pubDir = path.join(__dirname , './public')
 const viewsPath = path.join(__dirname , './templates/views')
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
-const port = process.env.port || 3010
+const port = process.env.port || 8000
 
 
 
@@ -25,13 +29,27 @@ app.use(express.static(pubDir))
 
 //--------------------------------------------------
 
-app.get("" , (req , res)=>{
-    res.render('index')
-})
+// app.get("" , (req , res)=>{
+//     res.render('index')
+// })
+
+
+// io.on('connection' , (socket)=>{
+//     console.log('New client connected')
+//     socket.emit('message' , 'Welcome')
+//     socket.on('message' , (retval)=>{
+//         console.log(retval)
+//     })
+
+// })
 
 
 
-app.listen(port , ()=>{
+
+
+
+//---------------------------------------------------
+server.listen(port , ()=>{
     console.log(`The server is up and running on port ${port}`)
 })
 
---
+++ public/js/chat.js
@@ -0,0 +1,13 @@
+const socket = io()
+
+// socket.on('message'  ,(mess)=>{
+//     console.log(mess)
+    
+    
+// })
+
+// document.getElementById('sendid').addEventListener('click' , ()=>{
+//    const retval =  document.getElementById('chatid').value
+//    console.log(retval)
+//    socket.emit('message' , retval)
+// })
